3241dab7406eba7644a7d7169268f59896f98053
fanhaoxuee <1470600398@qq.com>
2025-10-09 20:16:43 +0800

    109

--- paddle/fluid/inference/capi/pd_config.cc
+++ paddle/fluid/inference/capi/pd_config.cc
@@ -429,8 +429,3 @@ bool PD_IsValid(const PD_AnalysisConfig* config) {
 void PD_DisableGlogInfo(PD_AnalysisConfig* config) {
   config->config.DisableGlogInfo();
 }
-
-void PD_DeletePass(PD_AnalysisConfig* config, char* pass_name) {
-  config->config.pass_builder()->DeletePass(std::string(pass_name));
-}
-}  // extern "C"
